866d6d76eecbec0e441d0752917bfc52b73e97bf
Eaindra ShelBy <78012281+EaindraTinHla@users.noreply.github.com>
2023-03-23 16:08:35 +0630

    Update useSignup.js

--- src/hook/useSignup.js
+++ src/hook/useSignup.js
@@ -14,7 +14,7 @@ export const useSignup = () =>{
         setError(null)
 
 
-    const response = await fetch('http://localhost:5000/api/user/signup', {
+    const response = await fetch('https://react-quiz.onrender.com/api/user/signup', {
         method:"POST",
         headers:{'Content-Type' : 'application/json'},
         body:JSON.stringify({email, password})
